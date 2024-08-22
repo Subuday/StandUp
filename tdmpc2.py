dbc916dc60176a8a158cd2acb2fc7bb7f8941584
Subuday <mr.gigant977@gmail.com>
2024-08-22 22:15:13 +0300

    Implement encoder

--- tdmpc2.py
+++ tdmpc2.py
@@ -2,13 +2,22 @@ from dataclasses import dataclass
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torch import Tensor
 
 from utils import get_device_from_parameters
 
 @dataclass
 class TDMPC2Config:
-    latent_dim: int = 50
+    input_dim: int = 51
+
+    simplex_dim: int = 8
+
+    encoder_hidden_dim: int = 256
+    encoder_num_layers: int = 2
+
+    latent_dim: int = 512
+
     temporal_decay_coeff: float = 0.5
 
 class TDMPC2Policy(nn.Module):
@@ -94,5 +103,41 @@ class TDMPC2TOLD(nn.Module):
         pass
 
 class TDMPC2ObservationEncoder(nn.Module):
-    def __init__(self):
-        pass
+    def __init__(self, config: TDMPC2Config):
+        super().__init__()
+        self.layers = nn.Sequential(*TDMPC2ObservationEncoder._create_layers(config))
+
+    def _create_layers(config: TDMPC2Config) -> nn.ModuleList:
+        layers = nn.ModuleList()
+        layers.append(NormedLinear(config.input_dim, config.encoder_hidden_dim, act = nn.Mish(inplace = True)))
+        for _ in range(config.encoder_num_layers - 2):
+            layers.append(NormedLinear(config.encoder_hidden_dim, config.encoder_hidden_dim, act = nn.Mish(inplace = True)))
+        layers.append(NormedLinear(config.encoder_hidden_dim, config.latent_dim, act = SimNorm(V = config.simplex_dim)))
+        return layers
+    
+    def forward(self, x: Tensor) -> Tensor:
+        return self.layers(x) 
+
+class NormedLinear(nn.Linear):
+    def __init__(self, *args, act, dropout = None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.ln = nn.LayerNorm(self.out_features)
+        self.act = act
+        self.dropout = nn.Dropout(dropout, inplace=True) if dropout else None
+
+    def forward(self, x):
+        x = super().forward(x)
+        if self.dropout:
+            x = self.dropout(x)
+        return self.act(self.ln(x))
+    
+class SimNorm(nn.Module):
+    def __init__(self, V: int):
+        super().__init__()
+        self.V = V
+
+    def forward(self, x):
+        shape = x.shape
+        x = x.view(*shape[:-1], -1, self.V)
+        x = F.softmax(x, dim=-1)
+        return x.view(*shape)
