2e7052088d9d649104f9d6e6208b38ae5345a327
Subuday <mr.gigant977@gmail.com>
2024-08-28 23:49:18 +0300

    add ability to set seed

--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+import random
+
+import numpy as np
 import torch
 import torch.nn as nn
 
@@ -7,3 +10,14 @@ def get_device_from_parameters(module: nn.Module) -> torch.device:
     Note: assumes that all parameters have the same device
     """
     return next(iter(module.parameters())).device
+
+
+def set_global_seed(seed):
+    """Set seed for reproducibility."""
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
+    if torch.backends.mps.is_available():
+        torch.mps.manual_seed(seed)
