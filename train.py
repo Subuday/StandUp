635990d004e11651085a6e17a63483b2982284c9
Subuday <mr.gigant977@gmail.com>
2024-08-30 22:38:05 +0300

    prototype training process

--- train.py
+++ train.py
@@ -6,21 +6,62 @@ from env_dm_control import make_env
 from tdmpc2 import TDMPC2Policy, TDMPC2Config
 from utils import set_global_seed
 
+def to_episode_info(observation):
+    pass
+
 def train(
     env,
     policy,
-    training_steps
+    config,
 ):
     policy.train()
 
+    buffer = []
+
     step = 0
+    collected_episode_info = []
+    episode_done = True
     while True:
-        if step == training_steps:
+        if step == config.training_steps:
             break
 
+        if episode_done:
+            if len(collected_episode_info) > 0:
+                buffer.append(collected_episode_info)
+            
+            policy.reset()
+            observation = env.reset()
+            collected_episode_info = [to_episode_info(observation)]
+
+        if step > config.buffer_seed_size:
+            action = env.rand_act()
+            # action = policy.select_action(observation)
+        else:
+            action = env.rand_act()
+        observation, reward, episode_done, info = env.step(action)
+        collected_episode_info.append(to_episode_info(observation))
+            
+        print("Colllecting seed data... ", step)
+
+        if step < config.buffer_seed_size:
+            step += 1
+            continue
+
+        if step == config.buffer_seed_size:
+            iters = config.buffer_seed_size
+        else:
+            iters = 1
         
-    while step < training_steps:
-        pass
+        for _ in range(iters):
+            pass
+
+        if iters > 1:
+            print("training with seed data... ", step)
+        else:
+            print("training with new data... ", step)
+
+        step += 1
+
 
 
     batch = {
@@ -47,7 +88,7 @@ def main():
 
     policy = TDMPC2Policy(config).to("mps")
 
-    train(env, policy, config.training_steps)
+    train(env, policy, config)
 
 if __name__ == "__main__":
     main()
